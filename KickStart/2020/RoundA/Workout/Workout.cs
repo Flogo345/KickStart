a4fd82b464cd4b2a95a79452d6815508dd9f7668
Flogo345 <Flogo345@web.de>
2020-04-15 11:30:12 +0200

    added workout and getInput

---
+++ KickStart/2020/RoundA/Workout/Workout.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KickStart._2020.RoundA.Workout
+{
+    public static class Workout
+    {
+        public static void Solve()
+        {
+            var input = GetInput();
+            Console.WriteLine(input);
+        }
+
+        private static Tuple<int, int[], int[][]> GetInput()
+        {
+            int t = Convert.ToInt32(Console.ReadLine());
+            int[] additionalSessions = new int[t];
+            int[][] minutesLists = new int[t][];
+
+            for (int i = 0; i < t; i++)
+            {
+                string[] conditions = Console.ReadLine().Split(' ');
+                int n = Convert.ToInt32(conditions[0]);
+                int k = Convert.ToInt32(conditions[1]);
+
+                int[] minutes = new int[n];
+                var tempInput = Console.ReadLine().Split(' ');
+                for (int j = 0; j < n; j++)
+                {
+                    minutes[j] = Convert.ToInt32(tempInput[j]);
+                }
+                additionalSessions[i] = k;
+                minutesLists[i] = minutes;
+            }
+            return Tuple.Create(t, additionalSessions, minutesLists);
+        }
+    }
+}
--- KickStart/MainProgram.cs
+++ KickStart/MainProgram.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using KickStart._2020.RoundA.Allocation;
 using KickStart._2020.RoundA.Plates;
+using KickStart._2020.RoundA.Workout;
+
 
 namespace KickStart
 {
@@ -10,7 +12,7 @@ namespace KickStart
         static void Main(string[] args)
         {
             //Allocation.Solve(); //Aufgabe1
-            Plates.Solve();
+            Workout.Solve();
         }
     }
 }
